37358a827a7e533ff37de7e1c28c4635fbcbcfde
Saniya Tabssum <saniyatabssum23.set@modyuniversity.ac.in>
2026-08-19 16:07:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Multiplication Table/multiplication-table.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for(int i=1;i<=10;i++){
+        int sum=n*i;
+        cout<<sum<<" ";
+    }
+
+    return 0;
+}
