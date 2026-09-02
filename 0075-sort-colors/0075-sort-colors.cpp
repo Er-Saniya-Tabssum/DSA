6dc473b5327a818be2198cb17e878c499bdbf3d6
Saniya Tabssum <saniyatabssum23.set@modyuniversity.ac.in>
2026-09-02 11:42:16 +0530

    Time: 0 ms (100%), Space: 11.5 MB (98.14%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int i = 0;
+        int low=0;
+        int high=nums.size() - 1;
+        int j = nums.size() - 1;
+        while (i <= j) {
+            if (nums[i] == 0) {
+                int temp = nums[i];
+                nums[i] = nums[low];
+                nums[low] = temp;
+                i++;
+                low++;
+            } 
+            else if (nums[i] == 1) {
+                i++;
+            } 
+            else if (nums[i] == 2) {
+                int temp = nums[i];
+                nums[i] = nums[high];
+                nums[high] = temp;
+                j--;
+                high--;
+            }
+            else if (nums[j] == 2) {
+                int temp = nums[j];
+                nums[j] = nums[high];
+                nums[high] = temp;
+                j--;
+                high--;
+            }
+        }
+    }
+};
