c7e0302be7cb654671d82023ccf24e10a01ed8dc
Saniya Tabssum <saniyatabssum23.set@modyuniversity.ac.in>
2026-09-09 17:15:46 +0530

    Time: 0 ms (100%), Space: 43.7 MB (47.66%) - LeetHub

--- 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
@@ -7,27 +7,19 @@ public:
         int n = nums.size();
         int leastending = 0;
         int leastsum = INT_MAX;
-
         for (int i = 0; i < nums.size(); i++) {
-            csum = csum + nums[i];
-        }
-        for (int i = 0; i < nums.size(); i++) {
+             csum = csum + nums[i];
             int v1 = nums[i];
-            int v3 = best_ending + nums[i];
-            best_ending = max(v1, v3);
+            int v2 = best_ending + nums[i];
+            best_ending = max(v1, v2);
             sum = max(sum, best_ending);
-        }
-        for (int i = 0; i < nums.size(); i++) {
-            int v1 = nums[i];
             int v3 = leastending + nums[i];
             leastending = min(v1, v3);
             leastsum = min(leastsum, leastending);
-            cout << leastsum << " ";
         }
         if(sum<0){
             return sum;
         }
-        cout << csum << " csum : ";
         csum = csum - leastsum;
         return max(sum, csum);
     }
